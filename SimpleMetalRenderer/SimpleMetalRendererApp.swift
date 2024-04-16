5d7132636973b4218b651ab6141420cab659e69f
Alex Shepard <alex@meatfreezer.com>
2024-04-16 09:01:07 -0700

    simple metal rendering

---
+++ SimpleMetalRenderer/SimpleMetalRendererApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMetalRendererApp.swift
+//  SimpleMetalRenderer
+//
+//  Created by Alex Shepard on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMetalRendererApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
